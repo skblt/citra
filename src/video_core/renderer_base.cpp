d0193ab7d003b96b4df85d3e7a4e918c9d928091
emufan4568 <geoster3d@gmail.com>
2022-09-09 12:43:19 +0300

    renderer_opengl: Add driver class to report info/bugs

--- src/video_core/renderer_base.cpp
+++ src/video_core/renderer_base.cpp
@@ -5,28 +5,12 @@
 #include <memory>
 #include "core/frontend/emu_window.h"
 #include "video_core/renderer_base.h"
-#include "video_core/renderer_opengl/gl_rasterizer.h"
-#include "video_core/swrasterizer/swrasterizer.h"
-#include "video_core/video_core.h"
 
 RendererBase::RendererBase(Frontend::EmuWindow& window) : render_window{window} {}
 RendererBase::~RendererBase() = default;
 void RendererBase::UpdateCurrentFramebufferLayout(bool is_portrait_mode) {
     const Layout::FramebufferLayout& layout = render_window.GetFramebufferLayout();
     render_window.UpdateCurrentFramebufferLayout(layout.width, layout.height, is_portrait_mode);
-}
-
-void RendererBase::RefreshRasterizerSetting() {
-    bool hw_renderer_enabled = VideoCore::g_hw_renderer_enabled;
-    if (rasterizer == nullptr || opengl_rasterizer_active != hw_renderer_enabled) {
-        opengl_rasterizer_active = hw_renderer_enabled;
-
-        if (hw_renderer_enabled) {
-            rasterizer = std::make_unique<OpenGL::RasterizerOpenGL>(render_window);
-        } else {
-            rasterizer = std::make_unique<VideoCore::SWRasterizer>();
-        }
-    }
 }
 
 void RendererBase::Sync() {
